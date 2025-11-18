eb86d7668d69e0aa4b5dc2fdbf658741ad4e5b90
Isabelinhh <carmo.isabelly020@gmail.com>
2025-11-18 11:08:11 -0300

    Add files via upload

--- productRoutes.js
+++ productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const natalController = require('../controllers/productController');
 
 // Rotas de produtos
-router.get('/natal', productController.getAllProducts);
+router.get('/natal', productController.getAllnatal);
 router.get('/natal/:id', productController.getProductById);
 router.post('/natal', productController.createProduct);
 router.put('/natal/:id', productController.updateProduct);
